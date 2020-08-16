9e952f0b843a00624f2239e3f25f99dd2380f0dd
Luna <dlramos194@gmail.com>
2020-08-15 19:49:30 -0500

    added 5-day forecast data

--- script.js
+++ script.js
@@ -51,75 +51,88 @@ $.ajax({
         console.log(lon)
             //set ajax
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon,
+                url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+
+                //adding UV index Text
+                var colorIndex = $("<p>")
+                colorIndex.addClass("data")
+                colorIndex.attr("id", "uv-index")
+
+
+                $(colorIndex).text(response.value)
+                $("#current-city").append(colorIndex)
+                console.log(response.value)
+
+                var uvValue = response.value
+                    // CSS attributes to index value
+                if (uvValue < "3") {
+                    colorIndex.attr("style", "background-color:green")
+                }
+                if (uvValue >= "3" && uvValue <= "5.99") {
+                    colorIndex.attr("style", "background-color: gold")
+                }
+                if (uvValue >= "6" && uvValue <= "7.99") {
+                    colorIndex.attr("style", "background-color: orange")
+                }
+                if (uvValue >= "8" && uvValue <= "10.99") {
+                    colorIndex.attr("style", "background-color: red")
+                }
+                if (uvValue === "11" || uvValue > "11") {
+                    colorIndex.attr("style", "background-color:Indigo")
+                }
+            })
+            /* Getting the 5 day forecast*/
+        var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
+        $.ajax({
+            url: forecastURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
 
-            //adding UV index Text
-            var colorIndex = $("<p>")
-            colorIndex.addClass("data")
-            colorIndex.attr("id", "uv-index")
+            for (var i = 0; i < 5; i++) {
+                //creating our forecast elements
+                var cardforecast = $("<section>")
+                var Headerforecast = $("<h3>")
+                var icon = $("<img>")
+                var tempforecast = $("<p>")
+                var humidforecast = $("<p>")
 
 
-            $(colorIndex).text(response.value)
-            $("#current-city").append(colorIndex)
-            console.log(response.value)
+                //adding class to our forecast elements
+                cardforecast.addClass("card")
+                Headerforecast.addClass("forecast-date")
+                icon.addClass("forecast-icon")
+                tempforecast.addClass("forcast-data")
+                humidforecast.addClass("forecast-data")
 
-            var uvValue = response.value
-                // CSS attributes to index value
-            if (uvValue < "3") {
-                colorIndex.attr("style", "background-color:green")
+                // adding attributes to our forecast elements
+                icon.attr("src", " http://openweathermap.org/img/wn/" + icon + ".png")
+
+                //changing data to readable data
+                UnixTimestamp = (response.daily[i].dt)
+                milliseconds = UnixTimestamp * 1000
+                dateObject = new Date(milliseconds)
+                humanDateFormat = dateObject.toLocaleDateString("en-US", { month: "numeric", day: "numeric", year: "numeric" })
+                Farenheit = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
+                console.log(Farenheit)
+                    //adding text to our forecast elements
+                Headerforecast.text(humanDateFormat)
+                tempforecast.text(Farenheit.toPrecision(4))
+                humidforecast.text(response.daily[i].humidity)
+
+
+
+                //apending our orecast elements
+                $(".five-day-forecast").append(cardforecast)
+                $(cardforecast).append(Headerforecast)
+                $(cardforecast).append(icon)
+                $(cardforecast).append(tempforecast)
+                $(cardforecast).append(humidforecast)
             }
-            if (uvValue >= "3" && uvValue <= "5.99") {
-                colorIndex.attr("style", "background-color: gold")
-            }
-            if (uvValue >= "6" && uvValue <= "7.99") {
-                colorIndex.attr("style", "background-color: orange")
-            }
-            if (uvValue >= "8" && uvValue <= "10.99") {
-                colorIndex.attr("style", "background-color: red")
-            }
-            if (uvValue === "11" || uvValue > "11") {
-                colorIndex.attr("style", "background-color:Indigo")
-            }
-        })
+        });
 
 
     })
-
-/* Getting the 5 day forecast*/
-
-// var forecastURL = ""
-
-// $.ajax({
-//     url: forecastURL,
-//     method: "GET"
-// }).then(function(response) {
-//     console.log(response);
-
-//     //creating our forecast elements
-//     var cardforecast = $("<section>")
-//     var Hforecast = $("<h3>")
-//     var Pforecast = $("<p>")
-
-//     //adding class to our forecast elements
-//     cardforecast.addClass("card")
-//     Hforecast.addClass("forecast-date")
-//     Pforecast.addClass("forcast-data")
-
-//     //adding attributes to our forecast elements
-
-//     //adding text to our forecast elements
-//     Hforecast.text("")
-//     Pforecast.text("")
-//         //apending our orecast elements
-//     $(".five-day-forecast").append(cardforecast)
-//     console.log(cardforecast)
-
-//     console.log(response.list[0].dt_txt)
-
-
-// });
-
-// //
